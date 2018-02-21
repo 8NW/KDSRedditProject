517b3868ff60e1d6281836db8ffe03fce0f7294d
Noah VanSickle <noahvansickle@Noahs-MacBook-Pro.local>
2018-02-21 10:35:49 -0700

    GOOGLE OAUTH YES

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+
+require('./config/passport')(passport);
+
 
 var app = express();
+
+var index = require('./routes/index')(app, passport);
+var users = require('./routes/users');
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +30,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -57,6 +65,3 @@ module.exports = app;
 // });
 //
 // var Comment = mongoose.model('Comment', commentSchema);
-
-
-
