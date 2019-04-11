74356035b11dd3edd9f0196b45de0e51ce421fc5
Aiden Woodard <awoodard19@kentdenver.org>
2019-04-11 11:43:48 -0600

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
 
     'googleAuth' : {
         'clientID'         : '253990272152-kp2qqandphtelbj9b2e09vcgpilsk4fo.apps.googleusercontent.com',
-        'clientSecret'     : '0jTOQSPt9CDc0TiIoZ-6-xHM',
+        'clientSecret'     : '',
         'callbackURL'      : 'http://127.0.0.1:3000'
     },
 
